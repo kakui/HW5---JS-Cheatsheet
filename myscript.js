787e28bff57c4f34a8d8f29e44683f46b60752cf
Ka Kui Cheng Ye <kakuichengye@gmail.com>
2014-02-04 01:52:11 -0500

    modified JS file to remove spaces

--- myscript.js
+++ myscript.js
@@ -19,37 +19,37 @@ var cool_animals = ["dragon", "lion", "panda"];
 var lame_animals = ["chicken", "pig", "cow"];
 
 //Console
-console.log (x + y);
-console.log (stringHello);
-console.log (numberOne);
-console.log (cool_animals);
-console.log (lame_animals);
-console.log (cool_animals[0]);
+console.log(x + y);
+console.log(stringHello);
+console.log(numberOne);
+console.log(cool_animals);
+console.log(lame_animals);
+console.log(cool_animals[0]);
 /* Question for Zach: why won't console.log (cool_animals[0][2]); print dragon and panda? */
 
 //Testing
-console.log (numberOne === anotherOne);
-console.log (numberOne > anotherOne);
-console.log (stringHello === stringBye);
+console.log(numberOne === anotherOne);
+console.log(numberOne > anotherOne);
+console.log(stringHello === stringBye);
 
 //Logic
 if (x > y){
-	console.log ("x is greater than y");
+	console.log("x is greater than y");
 }else{
-	alert ("x is not greater than y dummy");
+	alert("x is not greater than y dummy");
 }
 
 //Functions
 function addFive(number){
 	return number + 5;
 }
-console.log (addFive(5));
+console.log(addFive(5));
 
 //3 Functions
 function guessTen(number){
-	if (number > 10){
+	if(number > 10){
 		alert("less than 10!");
-	}else if (number === 10){
+	}else if(number === 10){
 		alert("it is 10!");
 	}
 	else{
@@ -58,12 +58,12 @@ function guessTen(number){
 } 
 
 function guessName(name){
-	if (name === 'Ka Kui'){
-		console.log ("my name is Ka Kui!");
-	}else if (name === 'kakui'){
-		console.log ("add a space to " + name);
+	if(name === 'Ka Kui'){
+		console.log("my name is Ka Kui!");
+	}else if(name === 'kakui'){
+		console.log("add a space to " + name);
 	}else{
-		console.log ("that's not my name!");
+		console.log("that's not my name!");
 	}
 }
 
